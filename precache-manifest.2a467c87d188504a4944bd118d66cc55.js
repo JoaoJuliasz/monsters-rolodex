4601382e1a22edaf9508e72f1752575b8f4fd4f3
Joao Juliasz <joaojuliaszdemorais@gmail.com>
2020-02-17 12:43:24 -0300

    Updates

---
+++ precache-manifest.2a467c87d188504a4944bd118d66cc55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3864d6e9501ad2fb6c897242344c99c7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c69524c0901abd2c8e93",
+    "url": "/monsters-rolodex/static/css/main.946268b1.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69524c0901abd2c8e93",
+    "url": "/monsters-rolodex/static/js/main.4b93e4aa.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
